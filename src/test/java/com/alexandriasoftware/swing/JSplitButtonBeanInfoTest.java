4eb64996d5717dc03373ecf7854311135a3707d4
Randall Wood <297232+rhwood@users.noreply.github.com>
2022-05-07 12:30:15 -0400

    test: Add tests for JSplitButtonBeanInfo

---
+++ src/test/java/com/alexandriasoftware/swing/JSplitButtonBeanInfoTest.java
@@ -0,0 +1,66 @@
+package com.alexandriasoftware.swing;
+
+import static java.beans.BeanInfo.ICON_COLOR_16x16;
+import static java.beans.BeanInfo.ICON_COLOR_32x32;
+import static java.beans.BeanInfo.ICON_MONO_16x16;
+import static java.beans.BeanInfo.ICON_MONO_32x32;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.beans.BeanInfo;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+abstract class JSplitButtonBeanInfoTest {
+
+    protected BeanInfo info;
+    protected Class<?> trgt;
+
+    @BeforeEach
+    public void setUp() {
+        info = new JSplitButtonBeanInfo();
+        trgt = JSplitButton.class;
+    }
+
+    @Test
+    void testGetIcon() {
+        assertNotNull(info.getIcon(ICON_COLOR_16x16));
+        assertNotNull(info.getIcon(ICON_COLOR_32x32));
+        assertNotNull(info.getIcon(ICON_MONO_16x16));
+        assertNotNull(info.getIcon(ICON_MONO_32x32));
+        assertNull(info.getIcon(Integer.MAX_VALUE));
+    }
+
+    @Test
+    void TestGetBeanDescriptor() {
+        assertNotNull(info.getBeanDescriptor());
+        assertEquals(trgt, info.getBeanDescriptor().getBeanClass());
+    }
+
+    @Test
+    void TestGetPropertyDescriptors() {
+        assertNotNull(info.getPropertyDescriptors());
+    }
+
+    @Test
+    void TestGetEventSetDescriptors() {
+        assertNotNull(info.getEventSetDescriptors());
+    }
+
+    @Test
+    void TestGetMethodDescriptors() {
+        assertNotNull(info.getMethodDescriptors());
+    }
+
+    @Test
+    void TestGetDefaultPropertyIndex() {
+        assertEquals(-1, info.getDefaultPropertyIndex());
+    }
+
+    @Test
+    void TestGetDefaultEventIndex() {
+        assertEquals(-1, info.getDefaultEventIndex());
+    }
+}

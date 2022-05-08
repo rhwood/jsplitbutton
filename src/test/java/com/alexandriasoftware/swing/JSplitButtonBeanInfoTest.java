0aeef004d5334bd31cd37dd4ebb23c13b5b1ae64
Randall Wood <297232+rhwood@users.noreply.github.com>
2022-05-08 10:29:45 -0400

    fix: Refactor JSplitButtonBeanInfo (#73)
    * chore: format JSplitButtonBeanInfo code
    
    * chore: Refactor imports
    
    * test: Add tests for JSplitButtonBeanInfo
    
    * test: Tests should not be abstract to run
    
    * fix: remove support for Java 1.0 Event model
    
    This only removes the GUI for tying the JSplitButton into the Java 1.0 Event model, but does not prevent its use, so this is not considered a breaking change.
    
    * feat: new icons that work for color and mono
    
    * fix: remove dead code
    
    * fix: address critical issues
    
    * fix: address major issues flagged by Sonarcloud
    
    * fix: address minor issues found by SonarCloud
    
    * chore: get rid of most private constants
    
    * test: because Java 8
    
    * fix: remove unused private constants
    
    * test: include all classes in test coverage report

---
+++ src/test/java/com/alexandriasoftware/swing/JSplitButtonBeanInfoTest.java
@@ -0,0 +1,80 @@
+package com.alexandriasoftware.swing;
+
+import static java.beans.BeanInfo.ICON_COLOR_16x16;
+import static java.beans.BeanInfo.ICON_COLOR_32x32;
+import static java.beans.BeanInfo.ICON_MONO_16x16;
+import static java.beans.BeanInfo.ICON_MONO_32x32;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.beans.BeanInfo;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
+
+class JSplitButtonBeanInfoTest {
+
+    protected BeanInfo info;
+    protected Class<?> trgt;
+
+    @BeforeEach
+    public void setUp() {
+        info = new JSplitButtonBeanInfo();
+        trgt = JSplitButton.class;
+    }
+
+    @Test
+    void testGetIcon() {
+        assertNotNull(info.getIcon(ICON_COLOR_16x16));
+        assertNotNull(info.getIcon(ICON_COLOR_32x32));
+        assertNotNull(info.getIcon(ICON_MONO_16x16));
+        assertNotNull(info.getIcon(ICON_MONO_32x32));
+        assertNull(info.getIcon(Integer.MAX_VALUE));
+    }
+
+    @Test
+    void TestGetBeanDescriptor() {
+        assertNotNull(info.getBeanDescriptor());
+        assertEquals(trgt, info.getBeanDescriptor().getBeanClass());
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void TestGetPropertyDescriptorsOnJava8() {
+        assertNotNull(info.getPropertyDescriptors());
+        assertEquals(143, info.getPropertyDescriptors().length);
+    }
+
+    @DisabledOnJre(JRE.JAVA_8)
+    @Test
+    void TestGetPropertyDescriptors() {
+        assertNotNull(info.getPropertyDescriptors());
+        assertEquals(105, info.getPropertyDescriptors().length);
+    }
+
+    @Test
+    void TestGetEventSetDescriptors() {
+        assertNotNull(info.getEventSetDescriptors());
+        assertEquals(18, info.getEventSetDescriptors().length);
+    }
+
+    @Test
+    void TestGetMethodDescriptors() {
+        assertNotNull(info.getMethodDescriptors());
+        assertEquals(133, info.getMethodDescriptors().length);
+    }
+
+    @Test
+    void TestGetDefaultPropertyIndex() {
+        assertEquals(-1, info.getDefaultPropertyIndex());
+    }
+
+    @Test
+    void TestGetDefaultEventIndex() {
+        assertEquals(-1, info.getDefaultEventIndex());
+    }
+}

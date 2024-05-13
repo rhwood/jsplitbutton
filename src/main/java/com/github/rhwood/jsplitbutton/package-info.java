6d738149f2af73d97879de2b802541588e760b59
Randall Wood <297232+rhwood@users.noreply.github.com>
2024-05-13 07:13:02 -0400

    chore: consistent copyrights in comments

--- src/main/java/com/github/rhwood/jsplitbutton/package-info.java
+++ src/main/java/com/github/rhwood/jsplitbutton/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016, 2018 Randall Wood
+ * Copyright 2016, 2018, 2024 Randall Wood
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
